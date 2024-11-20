257fe0ed278e81be2d5d98760cdc0454962db0b4
FernandDev <juanfsanchezhoyos@gmail.com>
2024-11-19 20:21:28 -0500

    Cambios al diseño correctos, cambios necesarios

--- Templates/index_login.php
+++ Templates/index_login.php
@@ -1,6 +1,6 @@
 <?php
 $pagina = isset($_GET['p']) ? strtolower($_GET['p']) : 'inicio';
 
-require_once('inicio.php');
+require_once('../Templates/inicio.php');
 
 require_once '../Templates/' . $pagina . '.php';
